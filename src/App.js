f05bc35c813022b5975c35b432013b35e3c8b214
yatharth <yatharthverma070@gmail.com>
2021-07-21 12:23:41 +0530

    Initialize API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Results from "./components/Results";
 
 function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
+  const [apiResponse, setApiResponse] = useState();
+
+  useEffect(() => {
+    callAPI();
+  }, []);
+
+  const callAPI = async () => {
+    try {
+      const response = await fetch(
+        "https://run.mocky.io/v3/fefcfbeb-5c12-4722-94ad-b8f92caad1ad"
+      ).then((res) => res.json());
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return <Results />;
 }
 
 export default App;
